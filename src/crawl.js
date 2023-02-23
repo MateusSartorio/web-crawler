03a317304f5b33a6c279c9da411f899bfc5dd3f0
MateusSartorio <mateussartorio39@gmail.com>
2023-02-23 14:10:39 -0300

    getURLsFromHTML and tests

--- src/crawl.js
+++ src/crawl.js
@@ -1,3 +1,35 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(HTMLBody, baseURL) {
+    const urls = [];
+    
+    const dom = new JSDOM(HTMLBody).window.document;
+    const linkElements = dom.querySelectorAll("a");
+
+    for(linkElement of linkElements) {
+        if(linkElement.href[0] == "/") {
+            try {
+                const newURL = new URL(`${baseURL}${linkElement.href}`);
+                urls.push(normalizeURL(`${baseURL}${linkElement.href}`));
+            }
+            catch(e) {
+                console.log(`error with relative url: ${e.message}`);
+            }
+        }
+        else {
+            try {
+                const newURL = new URL(linkElement.href);
+                urls.push(normalizeURL(linkElement.href));
+            }
+            catch(e) {
+                console.log(`error with absolute url: ${e.message}`);
+            }
+        }
+    }
+
+    return urls;
+}
+
 function normalizeURL(urlString) {
     const trailingSlashRe = /\/$/;
 
@@ -8,5 +40,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 };
